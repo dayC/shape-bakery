61bdc56a8f07833687c62d7a71d8bf9bef1f0b3f
ggevoian <gevork.gevoian810@sie-1222-15.csuci.edu>
2016-09-07 14:40:55 -0700

    init commit

---
+++ src/UserInterface.java
@@ -0,0 +1,10 @@
+import javax.swing.JApplet;
+import javax.swing.SwingUtilities;
+import javax.swing.JLabel;
+import java.awt.*;
+
+public class UserInterface extends JApplet{
+    public UserInterface() throws HeadlessException {
+
+    }
+}
