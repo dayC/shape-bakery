17683a01c3de51d167c2586ec9ce3d610c18b143
Phillip Porter <phillip.porter54936@outlook.com>
2016-09-11 15:59:26 -0700

    Fucntions to control game length and shape cound
    Game length hard coded to 20 rounds. Shape count hard coded to 4 as
    planned.

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -6,6 +6,42 @@ public class GameEngine extends JApplet {
     Shape[] shapes = new Shape[]{Shape.CIRCLE, Shape.PENTAGON, Shape.RHOMBUS, Shape.TRAPEZOID};
     ArrayList<String> shapes2 = new ArrayList<String>();
 
+    // GAME_DURATION is 20 by default
+    private int gameDuration = 20;
+    private final int MIN_GAME_DURATION = 1;
+    private final int MAX_GAME_DURATION = 40;
+
+    // CAKE_SEQUENCE_LENGTH is 4 by default
+    private int cakeSequenceLength = 4;
+    private final int MIN_CAKE_SEQUENCE_LENGTH = 1;
+    private final int MAX_CAKE_SEQUENCE_LENGTH = 8;
+
+    private boolean setDuration(int turns)
+    {
+        if (turns >= MIN_GAME_DURATION && turns <= MAX_GAME_DURATION)
+        {
+            this.gameDuration = turns;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private boolean setCakeSequenceLength(int length)
+    {
+        if (length >= MIN_CAKE_SEQUENCE_LENGTH && length <= MAX_CAKE_SEQUENCE_LENGTH)
+        {
+            this.cakeSequenceLength = length;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     public void init() {
         try {
             SwingUtilities.invokeAndWait(new Runnable() {
