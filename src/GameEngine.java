5601af6f02352ffcabd410dc07898408901c1fb1
Phillip Porter <phillip.porter54936@outlook.com>
2016-09-11 19:26:30 -0700

    Modified GameEngine to match one in feat/UI
    GameEngine matches basic structure provided by @gevorkgevoian with added
    accessor methods and mutator methods for flow control.

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -2,11 +2,15 @@
  * Created by phillip.porter234 on 9/7/16.
  */
 
-import java.util.*;
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class GameEngine extends JApplet
 {
+    UI userInterface;
+    Shape[] shapes = new Shape[]{Shape.CIRCLE, Shape.PENTAGON, Shape.RHOMBUS, Shape.TRAPEZOID};
+    ArrayList<String> shapes2 = new ArrayList<String>();
+
     // GAME_DURATION is 20 by default
     private int gameDuration = 20;
     private final int MIN_GAME_DURATION = 1;
@@ -65,6 +69,30 @@ public class GameEngine extends JApplet
         {
             return false;
         }
+    }
+
+    public void init() {
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    resize(1024, 250);
+                    createGUI();
+                    userInterface.startRound(shapes);
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("createGUI didn't complete successfully");
+        }
+    }
+
+    private void createGUI() {
+        userInterface = new UI(this);
+        userInterface.setOpaque(true);
+        setContentPane(userInterface);
+    }
+
+    public void addShape(String shape) {
+        shapes2.add(shape);
     }
     
 }
